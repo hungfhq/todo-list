6dbe89d1896a70c1a6adfd7356ad2b407feca9c8
hunghq <hungfhq@gmail.com>
2019-09-26 01:02:26 +0700

    render a list use map method

--- src/App.js
+++ src/App.js
@@ -1,17 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 import TodoItem from './components/TodoItem';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <TodoItem title="Wash clothes" />
-        <TodoItem title="Empty trash bin" />
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.todoItems = [
+      'Wash clothes',
+      'Empty trash bin',
+      'Clean the bike'
+    ];
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          {
+            this.todoItems
+              .map((item, index) => <TodoItem key={index} title={item} />)
+          }
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
